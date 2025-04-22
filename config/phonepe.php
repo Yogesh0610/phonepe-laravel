8d937a0e5f9ac5f2f8fbc29d23887ee9ede4cd8e
Yogesh Gupta <86668869+Yogesh0610@users.noreply.github.com>
2025-04-22 18:07:58 +0530

    Update phonepe.php

--- config/phonepe.php
+++ config/phonepe.php
@@ -2,11 +2,11 @@
 
 return [
     'environment' => env('PHONEPE_ENV', 'uat'),
-
+    'redirect_url' => env('PHONEPE_REDIRECT_URL', 'http://127.0.0.1:8000/payment/verify'),
     'uat' => [
-        'client_id' => env('PHONEPE_UAT_CLIENT_ID', 'SU2504041946024365502022'),
+        'client_id' => env('PHONEPE_UAT_CLIENT_ID', 'TEST-M22ZNK7QX3FS6_25041'),
         'client_version' => env('PHONEPE_UAT_CLIENT_VERSION', 1),
-        'client_secret' => env('PHONEPE_UAT_CLIENT_SECRET', 'ae83cba2-07c0-43a9-b0c4-1d84c261fd12'),
+        'client_secret' => env('PHONEPE_UAT_CLIENT_SECRET', 'ZmEzZWU4MmQtMWIxOC00NTA4LWE2YzMtNjc5YjQ4YzlkY2Nl'),
         'base_url' => 'https://api-preprod.phonepe.com/apis/pg-sandbox',
         'auth_url' => 'https://api-preprod.phonepe.com/apis/pg-sandbox',
         'token_cache_path' => storage_path('app/phonepe/phonepe_token_uat.json'),
